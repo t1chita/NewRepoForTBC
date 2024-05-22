caa90b3acb2a27fbf35ea373a725d8927c51f20c
Temur Chitashvili <temur.chitashvilii@gmail.com>
2024-05-22 16:57:33 +0400

    MusicView Refactor

--- SwiftUI/SwiftUIExerciseFirst/SwiftUIExerciseFirst/Scenes/Subviews/MusicView.swift
+++ SwiftUI/SwiftUIExerciseFirst/SwiftUIExerciseFirst/Scenes/Subviews/MusicView.swift
@@ -10,54 +10,41 @@ import SwiftUI
 struct MusicView: View {
     @State var title: String = "We love property wrappers and closures"
     var body: some View {
-        ZStack{
-            Color.musicBackground
-                .ignoresSafeArea()
-                VStack {
-                    HStack {
-                        Button(action: {
-                            //TODO: Add Text Change Action
-                            title = "ჩვენ გვიყვარს ოთარ რამიშვილი"
-                        }, label: {
-                            Image("MicImage")
-                                .resizable()
-                                .frame(width: 32, height: 32)
-                        })
-                        .padding(EdgeInsets(top: 12, leading: 16, bottom: 0, trailing: 0))
-                        Spacer()
-                    }
-                    HStack {
-                        Text("ჯუზონები და რამე")
-                            .foregroundColor(.white)
-                            .frame(width: 87, height: 13)
-                            .font(.system(size: 9, weight: .regular))
-                            .padding(EdgeInsets(top: 2, leading: 16, bottom: 0, trailing: 0))
-                        Spacer()
-                    }
+        ZStack {
+            HStack {
+                VStack(alignment: .leading) {
+                    Button(action: {
+                        title = "ჩვენ გვიყვარს ოთარ რამიშვილი"
+                    }, label: {
+                        Image("MicImage")
+                            .resizable()
+                            .frame(width: 32, height: 32)
+                    })
+                    
+                    Text("ჯუზონები და რამე")
+                        .foregroundColor(.white)
+                        .frame(width: 87, height: 13)
+                        .font(.system(size: 9, weight: .regular))
                     Spacer()
                 }
-                ZStack {
-                    HStack {
-                        Spacer()
-                        Image("GuitarImage")
-                            .resizable()
-                            .frame(width: 82, height: 182)
-                            .padding(EdgeInsets(top: 50, leading: 16, bottom: 0, trailing: 0))
-                    }
-                    HStack {
-                        Text(title)
-                            .foregroundColor(.white)
-                            .frame(width: 130, height: 88)
-                            .font(.system(size: 17, weight: .bold))
-                            .lineLimit(19)
-                            .padding(EdgeInsets(top: 111, leading: 16, bottom: 0, trailing: 0))
-                        Spacer()
-                    }
-                }
+                Spacer()
+            }
+            .padding(EdgeInsets(top: 12, leading: 16, bottom: 0, trailing: 0))
+                Image("GuitarImage")
+                    .resizable()
+                    .frame(width: 82, height: 182)
+                    .padding(EdgeInsets(top: 50, leading: 80, bottom: 0, trailing: 0))
+                Text(title)
+                    .foregroundColor(.white)
+                    .frame(width: 130, height: 88)
+                    .font(.system(size: 17, weight: .bold))
+                    .lineLimit(19)
+                    .padding(EdgeInsets(top: 111, leading: 0, bottom: 33, trailing: 20))
         }
+        .background(Color.musicBackground)
     }
 }
 
 #Preview {
-    MusicView()
+    ContentView()
 }
