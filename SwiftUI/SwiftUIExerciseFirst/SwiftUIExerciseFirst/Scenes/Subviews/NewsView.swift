01b8e1b8696fa19ee71ac01f73a62dff12544ffa
Temur Chitashvili <temur.chitashvilii@gmail.com>
2024-05-22 17:38:39 +0400

    refactor newsview

--- SwiftUI/SwiftUIExerciseFirst/SwiftUIExerciseFirst/Scenes/Subviews/NewsView.swift
+++ SwiftUI/SwiftUIExerciseFirst/SwiftUIExerciseFirst/Scenes/Subviews/NewsView.swift
@@ -10,39 +10,27 @@ import SwiftUI
 struct NewsView: View {
     var body: some View {
         ZStack {
-            Color.newsBackground
-                .ignoresSafeArea()
-            ZStack {
-                VStack {
-                    HStack {
+            VStack(alignment: .leading) {
                         Button(action: {
                         }, label: {
                             Image(.person)
                                 .resizable()
                                 .frame(width: 32, height: 32)
                         })
-                        Spacer()
-                    }
-                    HStack {
                         Text("ცეცხლოვანი სიახლეები")
                             .foregroundColor(.white)
                             .frame(width: 61, height: 26)
                             .font(.system(size: 9, weight: .regular))
                             .lineLimit(2)
-                        Spacer()
-                    }
-                    Spacer()
                 }
-                .padding(EdgeInsets(top: 12, leading: 16, bottom: 0, trailing: 0))
-                HStack {
-                    Spacer()
+                .padding(EdgeInsets(top: 12, leading: 16, bottom: 38, trailing: 88))
                     Image(.rupori)
                         .resizable()
                         .frame(width: 66, height: 86)
-                }
-                .padding(EdgeInsets(top: 24, leading: 0, bottom: 0, trailing: 0))
-            }
+                .padding(EdgeInsets(top: 24, leading: 99, bottom: 0, trailing: 0))
         }
+        .background(Color.newsBackground)
+        .cornerRadius(18)
     }
 }
 
